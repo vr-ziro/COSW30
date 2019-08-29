c85716ec4dd23549729a7ae76f0370f28142f125
EC2 Default User <ec2-user@ip-172-31-33-68.ec2.internal>
2019-08-29 02:58:26 +0000

    created lab1 file and added cat ipsum

---
+++ Labwork/Lab1/lab1.php
@@ -0,0 +1,15 @@
+<!DOCYTPE html>
+<html> 
+    <head>
+        <title>My 1st PHP Page!</title>
+    </head>
+    <body>
+        <h1>Victor Rodriguez</h1>
+        <p>Drool somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock cats woo or sit and stare lick arm hair. Destroy house in 5 seconds curl up and sleep on the freshly laundered towels chew master's slippers but hit you unexpectedly ignore the human until she needs to get up, then climb on her lap and sprawl, yet bite off human's toes. Dismember a mouse and then regurgitate parts of it on the family room floor sit in window and stare oooh, a bird, yum i vomit in the bed in the middle of the night sleep everywhere, but not in my bed yet shake treat bag, yet sleep on dog bed, force dog to sleep on floor. Being gorgeous with belly side up pet me pet me pet me pet me, bite, scratch, why are you petting me chase the pig around the house or the cat was chasing the mouse.</p>
+        
+        <?php
+            echo 'This is my beautiful paragraph.';
+        ?>
+        
+    </body>
+</html>
