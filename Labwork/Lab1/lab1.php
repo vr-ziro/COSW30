12a4bfed59f76233a6f1c50ba2029379de30f824
EC2 Default User <ec2-user@ip-172-31-33-68.ec2.internal>
2019-09-01 06:44:06 +0000

    Added a few CSS Properties and PHP Comment

--- Labwork/Lab1/lab1.php
+++ Labwork/Lab1/lab1.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-/* File: lab1.php Name: Victor Rodriguez, date: 8-31-2019 */
 <html> 
     <head>
         <title>Labwork 1: Hello World</title>
     </head>
+    
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
+        body {
+            background-color:#0A2342;
+            color: #FFFDF7;
+            font-family: 'Ubuntu', sans-serif;
+        }
+    </style>
+    
     <body>
         <h1>Hi There!</h1>
         <p>Hello, my name is Victor Rodriguez and I seriously enjoy art and tech. Ever since I can remember I have always been interested in how technology worked as well as the performing arts. Ended up learning a few instruments and am now picking up coding. P.S my GitHub profile can be found <a href="https://github.com/vr-ziro">here.</a></p>
@@ -16,6 +25,7 @@
         </ul>
         
         <?php
+        /* File: lab1.php Name: Victor Rodriguez, date: 8-31-2019 */
             echo '<p>Hmmm, along the lines of websites that I use, it would probably have to be between <a href="https://www.youtube.com/">YouTube</a>, <a href="https://www.netflix.com/browse">Netflix</a>, and <a href="https://stackoverflow.com/">Stack Overflow</a>. Mainly because I tend to watch alot of shows to relax and look up alot of problems when coding.<p>';
             echo '<p> As a whole, this assignment was not too bad, honesly pretty cool to think I have 3 different languages within 1 file. All in all, I am looking forward to what PHP has to offer.'
         ?>
